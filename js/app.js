1a531f7af6ad19391660dad10d14d9d3afc37a50
Daniel Verdejo <Daniel Verdejo>
2016-10-12 22:47:19 +0100

    Commited by Daniel: initial commit, javascript & html, files, folders, & libraries required for development

---
+++ js/app.js
@@ -0,0 +1,2 @@
+/*Created by Daniel Verdejo on 11/10/2016.*/
+var app = angular.module('projectApp', ["projectCtrlModule", "ngRoute"]);
---
+++ js/controllers/projectCtrl.js
@@ -0,0 +1,14 @@
+/*  Created by Daniel Verdejo 11/10/2016.
+    In the following script we will store
+    all controllers & services.
+    */
+angular.module('projectCtrlModule', [])
+
+.controller("projectCtrl",["$scope", function($scope){
+    
+    
+}]),
+//Using a service which will only have one instance, this allow us to abstract use and functionality across application
+.factory("doStuff", function(){
+    
+});
